8d9b9b6613e582fdd647ed831a212b8715b09b04
Edward-Kabue <ekabue0@gmail.com>
2024-04-15 17:44:40 +0300

    Update profile_screen.dart and bottom_nav.dart for profile screen and bottom navigation functionality

--- lib/presentation/home/profile_screen.dart
+++ lib/presentation/home/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotel/presentation/home/widgets/bottom_nav.dart';
 import 'package:provider/provider.dart';
 import 'package:hotel/providers/auth_provider.dart';
 import 'package:hotel/domain/models/user_model.dart';
@@ -10,20 +11,20 @@ class ProfileScreen extends StatelessWidget {
     final authProvider = Provider.of<AuthProvider>(context, listen: true);
     final UserModel? user = authProvider.user;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome, ${user?.email ?? 'Guest'}',
-              style: const TextStyle(fontSize: 20),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Profile'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome, ${user?.email ?? 'Guest'}',
+                style: const TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: const BottomBar());
   }
 }
---
+++ lib/presentation/home/widgets/bottom_nav.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      showSelectedLabels: true,
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.message),
+          label: 'Messages',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'Profile',
+        )
+      ],
+    );
+  }
+}
