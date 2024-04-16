eee0d9adffabe69ccab685f210cfbf529e4c30ee
Edward-Kabue <ekabue0@gmail.com>
2024-04-16 13:37:10 +0300

    Update profile screen UI and add TextWidget component

--- lib/presentation/home/profile_screen.dart
+++ lib/presentation/home/profile_screen.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:hotel/presentation/home/widgets/text_widget.dart';
 import 'package:provider/provider.dart';
 import 'package:hotel/providers/auth_provider.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
 import 'package:hotel/domain/models/user_model.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -11,20 +15,116 @@ class ProfileScreen extends StatelessWidget {
     final UserModel? user = authProvider.user;
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
         title: const Text('Profile'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome, ${user?.email ?? 'Guest'}',
-              style: const TextStyle(fontSize: 20),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RichText(
+                text: TextSpan(
+                  text: 'Hi,  ',
+                  style: const TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: user!.email,
+                        style: const TextStyle(
+                          color: Colors.cyan,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            print('My name is pressed');
+                          }),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              TextWidget(
+                text: 'Email@email.com',
+                color: Colors.cyan,
+                textSize: 18,
+                // isTitle: true,
+              ),
+              const Divider(
+                thickness: 2,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              _listTiles(
+                title: 'Address 2',
+                subtitle: 'My subtitle',
+                icon: IconlyLight.profile,
+                onPressed: () {},
+                color: Colors.cyan,
+              ),
+              _listTiles(
+                title: 'Bookings',
+                icon: IconlyLight.bag,
+                onPressed: () {},
+                color: Colors.cyan,
+              ),
+              _listTiles(
+                title: 'Viewed',
+                icon: IconlyLight.show,
+                onPressed: () {},
+                color: Colors.cyan,
+              ),
+              _listTiles(
+                title: 'Forget password',
+                icon: IconlyLight.unlock,
+                onPressed: () {},
+                color: Colors.cyan,
+              ),
+              _listTiles(
+                title: 'Logout',
+                icon: IconlyLight.logout,
+                onPressed: () {},
+                color: Colors.cyan,
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _listTiles({
+    required String title,
+    String? subtitle,
+    required IconData icon,
+    required Function onPressed,
+    required Color color,
+  }) {
+    return ListTile(
+      title: TextWidget(
+        text: title,
+        color: color,
+        textSize: 22,
+        // isTitle: true,
+      ),
+      subtitle: TextWidget(
+        text: subtitle == null ? "" : subtitle,
+        color: color,
+        textSize: 18,
+      ),
+      leading: Icon(icon),
+      trailing: const Icon(IconlyLight.arrowRight2),
+      onTap: () {
+        onPressed();
+      },
     );
   }
 }
